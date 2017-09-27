29d262e2b64754503e8f87e4a9f8651801d85f53
phamhungdg96 <phamhungdg96@gmail.com>
2017-09-27 08:43:08 +0700

    add event
    thêm sự kiện cho button1, button2

--- QLHSGV/usr_TTGD.cs
+++ QLHSGV/usr_TTGD.cs
@@ -45,16 +45,63 @@ namespace QLHSGV._TTGD
         {
             
         }
-
+        //duc hung
         private void button1_Click(object sender, EventArgs e)
         {
           
-           
+           ThongTinGiangDay gv = new ThongTinGiangDay();
+            bool add = false;
+            try
+            {
+                gv.MaKip = textBox1.Text;
+                gv.MaGV = new GiaoVienDAO().GetIDbyName(comboBox1.Text);
+                gv.SoTiet = Convert.ToInt32(textBox3.Text);
+                gv.TenLop = comboBox2.Text;
+                gv.MonHoc = textBox2.Text;
+                gv.Ngay = dateTimePicker1.Value;
+                add = new ThongTinGiangDayDAO().Insert(gv);
+            }
+            catch
+            {
+
+            }
+            if (!add) MessageBox.Show("Mã kíp đã tồn tại", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            else
+            {
+                MessageBox.Show("Đã thêm lịch", "Thành công", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                dtg_TTGD.DataSource = new ThongTinGiangDayDAO().ListAll();
+                textBox1.Text = "";
+                textBox2.Text = "";
+                textBox3.Text = "";
+            }
+
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-           
+           ThongTinGiangDay gv = new ThongTinGiangDay();
+            bool edit =false;
+            try
+            {
+                gv.MaKip = textBox1.Text;
+                gv.MaGV = new GiaoVienDAO().GetIDbyName(comboBox1.Text);
+                gv.SoTiet = Convert.ToInt32(textBox3.Text);
+                gv.TenLop = comboBox2.Text;
+                gv.MonHoc = textBox2.Text;
+                gv.Ngay = dateTimePicker1.Value;
+                edit = new ThongTinGiangDayDAO().Update(gv);
+            }
+            catch
+            {
+
+            }
+            if (!edit) MessageBox.Show("Không tồn tại kíp", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            else
+            {
+                MessageBox.Show("Đã sửa", "Thành công", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                dtg_TTGD.DataSource = new ThongTinGiangDayDAO().ListAll();
+            }
+
         }
 
         private void button3_Click(object sender, EventArgs e)
